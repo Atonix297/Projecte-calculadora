4b18e84e42383cbfd0d972ec835374c4c85a1584
Pierangelo Sánchez <pierangelosanchez123@gmail.com>
2026-09-24 11:30:23 +0200

    modificación calculadora

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print("Hola calculadora")
+print("Hola, bienvenido a la calculadora!")
 def sumar(a, b):
     return a + b
 
-print("Suma 5 + 3 =", sumar(5, 3))
+print("La suma de 5 + 3 =", sumar(5, 3))
+print("Se acabó el programa:")
+print("Adiós!")
